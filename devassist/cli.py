d9f99db1aae05d6f5208ed726b628ee9fcef5bfd
isushmeeta <sushmitah0199@gmail.com>
2026-01-11 00:03:55 +0600

    Add: CLI entry point

--- devassist/cli.py
+++ devassist/cli.py
@@ -12,4 +12,10 @@ class DevAssist:
     def init(self, name:str):
 
         console.print(f"[green] Initilaizing project : [/green] {name}")
+        Project().create(name)
 
+def main():
+    fire.Fire(DevAssist)
+
+if __name__== "__main__":
+    main()
