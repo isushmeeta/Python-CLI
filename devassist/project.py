fda072389ae4e7b86d37d8db51b0e56019acc4a7
isushmeeta <sushmitah0199@gmail.com>
2026-01-13 23:43:25 +0600

    update: project.py

--- devassist/project.py
+++ devassist/project.py
@@ -6,21 +6,27 @@ console=Console()
 
 
 class Project:
-    def create(self, name:str):
+    def create(self, name:str, git:bool=True):
         project_path =Path(name)
 
         if project_path.exists():
             console.print("[red]Error:[/red]Projects already exists")
+            return
         #structure
         project_path.mkdir()
         (project_path / 'src').mkdir()
         (project_path / 'README.md').touch() # touch() creaes and empty file
 
         #git intialiazation
-        subprocess.run(['git','init', str(project_path)],
-                       stdout=subprocess.DEVNULL,
-                       stderr=subprocess.DEVNULL)
+
+        if git:
+            subprocess.run(
+                ['git','init', str(project_path)],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL
+            )
+            console.print("Git Initialized")
         
-        console.print('[bold green] Project Created Successfully [/bold green]')
-        console.print(f'{project_path}/')
-        console.print('Git Initialized')
+            console.print('[bold green] Project Created Successfully [/bold green]')
+            console.print(f'{project_path}/')
+            
